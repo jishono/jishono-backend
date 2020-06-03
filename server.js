6718d792dc33f921f82c7aed685705a358ba6fed
Per Gunnar Paulsen <pergpau@gmail.com>
2020-06-03 18:46:26 +0200

    fixed config and cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+/* const cors = require("cors"); */
 const app = express();
 const middleware = require("./app/routes/middleware");
 
