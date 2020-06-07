e58f6aa2f46c7a4ae45ea6dbcdaec474952fde7d
Per Gunnar Paulsen <pergpau@gmail.com>
2020-06-07 13:31:58 +0200

    cors

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.use(cors(corsOptions)); */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://admin.jisho.no"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(function (req, res, next) {
   if (req.url == '/login') {
     next()
