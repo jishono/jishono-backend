f097ed2ffe87f2c865234577a0bdb2272fe082fd
Per Gunnar Paulsen <pergpau@gmail.com>
2020-08-05 15:52:19 +0200

    fikse oversettelser

--- server.js
+++ server.js
@@ -11,12 +11,19 @@ if (config.app.node_env == 'development') {
     origin: "http://localhost:8080"
   }
   app.use(cors(corsOptions))
-} 
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./app/routes/routes")(app);
+
+app.use((req, res, next) => {
+  res.status(404).send({
+    status: 404,
+    error: 'URLen finnes ikke'
+  })
+})
 
 app.get("/", (req, res) => {
   res.json({ message: "jisho.no admin-api" });
